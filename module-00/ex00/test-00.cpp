fc39dd9d75a67c02053439528a17549650b3a396
Anton <ivnvtosh@icloud.com>
2022-10-20 01:50:06 +0300

    Commit 16

--- module-00/ex00/test-00.cpp
+++ module-00/ex00/test-00.cpp
@@ -6,7 +6,7 @@
 /*   By: ccamie <ccamie@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 01:15:21 by ccamie            #+#    #+#             */
-/*   Updated: 2022/10/20 01:49:11 by ccamie           ###   ########.fr       */
+/*   Updated: 2022/10/20 01:50:04 by ccamie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	main(void) {
 	std::getline(std::cin, returned);
 
 	if (returned == expected) {
-		std::cout << "\x1b[32mTEST 00 SUCCESS\x1b[0m\n";
-		std::cout << "\x1b[37mExpected: " << expected << "\x1b[0m\n";
-		std::cout << "\x1b[37mReturned: " << returned << "\x1b[0m\n";
+		std::cout << "\x1b[34mTEST 00 SUCCESS\x1b[0m\n";
+		std::cout << "\x1b[35mExpected: " << expected << "\x1b[0m\n";
+		std::cout << "\x1b[36mReturned: " << returned << "\x1b[0m\n";
 		return EXIT_SUCCESS;
 	} else {
 		std::cout << "\x1b[31mFAILURE\x1b[0m\n";
