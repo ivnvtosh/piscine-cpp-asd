c0d146e758201fb64834ea264108f9a63d9269ae
Anton <ivnvtosh@icloud.com>
2022-10-20 01:46:30 +0300

    Commit thireth

--- module-00/ex00/test-00.cpp
+++ module-00/ex00/test-00.cpp
@@ -6,7 +6,7 @@
 /*   By: ccamie <ccamie@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 01:15:21 by ccamie            #+#    #+#             */
-/*   Updated: 2022/10/20 01:42:07 by ccamie           ###   ########.fr       */
+/*   Updated: 2022/10/20 01:46:23 by ccamie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	main(void) {
 
 	if (returned == expected) {
 		std::cout << "\x1b[32mTEST 00 SUCCESS\x1b[0m\n";
-		std::cout << "Expected: " << expected << std::endl;
-		std::cout << "Returned: " << returned << std::endl;
+		std::cout << "\x1b[32mExpected: " << expected << "\x1b[0m\n";
+		std::cout << "\x1b[32mReturned: " << returned << "\x1b[0m\n";
 		return EXIT_SUCCESS;
 	} else {
 		std::cout << "\x1b[31mFAILURE\x1b[0m\n";
-		std::cout << "Expected: " << expected << std::endl;
-		std::cout << "Returned: " << returned << std::endl;
+		std::cout << "\x1b[32mExpected: " << expected << "\x1b[0m\n";
+		std::cout << "\x1b[32mReturned: " << returned << "\x1b[0m\n";
 		return EXIT_FAILURE;
 	}
 }
